098addae2bfa039ea887d73e248f96149d8c57b5
Claire Mathiron <cmathiron@inow.fr>
2023-04-21 00:25:45 +0200

    articlesList in Cards and more css

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,3 @@
-<!-- <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import 'primeflex/primeflex.css';
-import Button from 'primevue/button';
-// import { useAuthenticationStore } from './stores/authStore';
-import router from "./router/index";
-
-
-let notConnected: boolean = true
-// let { token } = useAuthenticationStore();
-
-const logout = () => {
-  console.log("bouton cliqué")
-  localStorage.removeItem("token");
-}
-
-const goToRegister = () => {
-  console.log("bouton register cliqué")
- router.push({ path: '/' });
-}
-const goToLogin = () => {
-  router.push({ path: '/login' });
-}
-const goToBlog = () => {
-  router.push({ path: '/articles' });
-}
-const goToProfile = () => {
-  router.push({ path: '/profile' });
-}
-
-
-</script>
-
-<template>
-  <div class="flex-column align-items-center">
-    <header class="flex flex-column align-items-center justify-content-center  w-full">
-      <div class="flex justify-content-center align-items-center w-full">
-        <img alt="Vue logo" class=" mr-4" src="@/assets/logo.svg" width="125" height="125" />
-        <img alt="Typesript logo" class="ml-4" src="@/assets/typescriptIcon.png" width="125" height="125" />
-      </div>
-
-
-      <nav class="mt-6">
-          <RouterLink to="/">Créer un compte |</RouterLink>
-          <RouterLink to="/login" class="ml-2">Se connecter |</RouterLink>
-          <RouterLink to="/login" @click="logout" class="ml-2"> Se déconnecter |</RouterLink>
-          <RouterLink to="/articles" class="ml-2">Blog |</RouterLink>
-          <RouterLink to="/profile" class="ml-2">Profil |</RouterLink>
-  
-         <Button label=" Créer un compte" link @click="goToRegister"/>
-        <Button label=" Se connecter" link @click="goToLogin" />
-        <Button label=" Se déconnecter" link @click="logout" />
-        <Button label="Blog" link @click="goToBlog"/>
-        <Button label="Profil" link @click="goToProfile"/> -->
-
-     <!--  </nav>
-
-    </header>
-
-    <RouterView />
-  </div>
-</template>
-
-<style scoped>
-nav button {
-  text-decoration: none;
-}
-
-nav button:hover {
-  text-decoration: underline;
-  font-weight: bold;
-  font-size: 17px;
-}
-</style> -->
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import 'primeflex/primeflex.css';
@@ -93,7 +19,7 @@ const logout = () => {
     <header class="flex flex-column align-items-center justify-content-center  w-full">
       <div class="flex justify-content-center align-items-center w-full">
         <img alt="Vue logo" class=" mr-4" src="@/assets/logo.svg" width="125" height="125" />
-        <img alt="Typesript logo" class=" ml-4 rounded" src="@/assets/typescriptIcon.png" width="125" height="125" />
+        <img alt="Typesript logo" class=" ml-4 border-round" src="@/assets/typescriptIcon.png" width="125" height="125" />
       </div>
 
 
--- src/views/ArticlesListView.vue
+++ src/views/ArticlesListView.vue
@@ -7,8 +7,10 @@ import 'primeicons/primeicons.css';
 import { type Ref, ref, onMounted } from 'vue';
 import Card from 'primevue/card';
 import Button from 'primevue/button';
-import Dropdown from 'primevue/dropdown';
+import RadioButton from 'primevue/radiobutton';
 import router from "../router/index";
+import DataView from 'primevue/dataview';
+import DataViewLayoutOptions from 'primevue/dataviewlayoutoptions'
 
 const selectedCity = ref();
 const cities = ref([
@@ -45,29 +47,56 @@ const goToAddArticle = () => {
 <template>
   <div>
     <!--  <RouterLink to="/writeArticle" @click="goToAddArticle">+ Ajouter un article</RouterLink>
-          <Button label="+ Ajouter un article" link @click="goToAddArticle" /> -->
-    <div class="my-6 flex justify-content-between">
-      <div></div>
-      <h1 class=" text-center">Retrouvez nos articles</h1>
-      <Button type="button" label="Nouvel article" icon="pi pi-pencil" @click="goToAddArticle" />
-    </div>
-    <span class="p-float-label my-4 w-full ">
+                    <Button label="+ Ajouter un article" link @click="goToAddArticle" /> -->
+    <div class="flex align-items-end justify-content-end">
+      <div class="my-6 flex justify-content-between w-7 bg-pink-100">
 
-
-      <Dropdown id="continent" v-model="tag" :options="cities" optionLabel="name" placeholder="Select a continent"
-        class="w-full md:w-14rem" />
-      <label for="continent">Continent</label>
-    </span>
-    <div v-for="article in articlesList" :key:string="articlesList">
-
-
-      <div v-for="item in article" class="bg-cyan-100 mb-8 flex-column justify-content-center align-items-evenly">
-        <h2 class="text-center mb-4 bg-pink-100 font-bold">{{ item.titre }}</h2>
-        <div class="bg-yellow-100 w-full flex">
-          <img :src=item.photo class="align-self-center justify-self-center rounded w-4 mb-4 mb-4 bg-purple-100" />
-        </div>
-        <p class="text-justify bg-purple-100">{{ item.description }}</p>
+        <h1 class=" text-center">Retrouvez nos articles</h1>
+        <Button type="button" label="Nouvel article" icon="pi pi-pencil" @click="goToAddArticle" />
       </div>
     </div>
+    <div class="bg-orange-100 flex-column align-items-center">
+    <div class="flex flex-wrap gap-6 mb-4 bg-teal-200 w-8 align-self-center">
+      <div class="flex align-items-center">
+        <RadioButton v-model="tag" inputId="afrique" name="afrique" value="afrique" />
+        <label for="afrique" class="ml-2">Afrique</label>
+      </div>
+      <div class="flex align-items-center">
+        <RadioButton v-model="tag" inputId="amerique" name="amerique" value="amerique" />
+        <label for="amerique" class="ml-2">Amerique</label>
+      </div>
+      <div class="flex align-items-center">
+        <RadioButton v-model="tag" inputId="oceanie" name="oceanie" value="oceanie" />
+        <label for="oceanie" class="ml-2">Oceanie</label>
+      </div>
+      <div class="flex align-items-center">
+        <RadioButton v-model="tag" inputId="europe" name="europe" value="europe" />
+        <label for="europe" class="ml-2">Europe</label>
+      </div>
+      <div class="flex align-items-center">
+        <RadioButton v-model="tag" inputId="asie" name="asie" value="asie" />
+        <label for="asie" class="ml-2">Asie</label>
+      </div>
+    </div>
+    <div v-for="article in articlesList" :key:string="articlesList"
+      class="flex-column justify-content-center align-items-center">
+
+
+      <Card v-for="item in article"
+        class="w-8 bg-cyan-100 mb-8 flex-column justify-content-center align-items-between ">
+
+        <template #content>
+          <h2 class="text-center mb-4 bg-pink-100 font-bold w-full">{{ item.titre }}</h2>
+
+          <div class="bg-yellow-100 w-full flex justify-content-center border-round">
+            <img :src=item.photo class="align-self-center justify-self-center border-round w-4 mb-4 mb-4 bg-purple-100" />
+          </div>
+          <p class="text-justify bg-purple-100">{{ item.description }}</p>
+        </template>
+      </Card>
+
   </div>
-</template>
+
+
+  </div>
+</div></template>
